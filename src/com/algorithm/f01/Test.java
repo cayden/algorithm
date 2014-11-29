370d96f47c717e522a8d0a0b2d3842d52cf3580e
cuiran <cuiran2001@163.com>
2014-11-29 17:18:43 +0800

    add

---
+++ src/com/algorithm/f01/Test.java
@@ -0,0 +1,25 @@
+/**
+ * Test.java
+ * 版权所有(C) 2014
+ * 创建者:cuiran 2014-11-29 下午5:18:33
+ */
+package com.algorithm.f01;
+
+/**
+ * TODO 
+ * @author cuiran
+ * @version 1.0.0
+ */
+public class Test {
+
+	/**
+	 * 2014-11-29 下午5:18:33
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
