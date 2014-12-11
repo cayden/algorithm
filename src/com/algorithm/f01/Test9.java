7d7cefb755ef2870ce8af3bf5b151920a14d4f88
cuiran <cuiran2001@163.com>
2014-12-11 21:56:22 +0800

    add

---
+++ src/com/algorithm/f01/Test9.java
@@ -0,0 +1,15 @@
+package com.algorithm.f01;
+
+public class Test9 {
+
+	/**
+	 * 2014-12-11 обнГ9:55:20
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
