69257cb461453e9fd23caa5ef967f3679438a80c
cuiran <cuiran2001@163.com>
2014-12-11 21:51:44 +0800

    add class

---
+++ src/com/algorithm/f01/TestFirst.java
@@ -0,0 +1,15 @@
+package com.algorithm.f01;
+
+public class TestFirst {
+
+	/**
+	 * 2014-12-11 обнГ9:51:12
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
